742d95d0c29a775d298a766fec20013bdf937c4a
Michael Blix <m.blix@samsung.com>
2020-04-09 00:06:32 -0700

    Rename files

--- tests.js
+++ tests.js
@@ -1,34 +1,17 @@
-'use strict';
-
-const VERSION = 0.1;
-console.log('XR Tests ' + VERSION);
-
-let objs = Object.getOwnPropertyNames(window).sort(function(a,b){return a.length - b.length;});
-let xrObjs = objs.filter(function(a){ return a.startsWith('XR') });
-console.log(xrObjs);
-
-let $ = document.querySelector.bind(document);
-let testOutput = $('#test-output');
-
-
-for (let i = 0; i < tests.length; i++) {
-  let test = tests[i];
-  let testF = test.f;
-  let desc = test.desc;
-
-  let _result = testF();
-  console.log(_result);
-
-  let result;
-  if (_result === test.expect) {
-    result = '<span class="pass">PASS</span>';
-  } else {
-    result = '<span class="fail">FAIL</span>';
-    // TODO: add in expect message
-  }
-
-  let el = document.createElement('div');
-  el.innerHTML = `${desc}: ${result}`;
-
-  testOutput.appendChild(el);
+// TESTS
+let tests = [
+{
+  desc: '"<code>XRSystem</code>" object in <code>window</code> object',
+  f: function() {
+    return 'XRSystem' in window
+  },
+  expect: true
+},
+{
+  desc: '"<code>XR</code>" object NOT in <code>window</code> object',
+  f: function() {
+    return 'XR' in window
+  },
+  expect: false
 }
+];
