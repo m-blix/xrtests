3da6d5dc3fa42461f6f4366c2cdb0bc5b2955747
Michael Blix <m.blix@samsung.com>
2020-04-09 17:17:14 -0700

    Add test for other XR window props

--- tests.js
+++ tests.js
@@ -13,5 +13,25 @@ let tests = [
     return 'XR' in window
   },
   expect: false
+},
+{
+  // "XRSystem"
+  desc: 'All other XR objects in <code>window</code> object',
+  f: function() {
+    let objs = Object.getOwnPropertyNames(window).sort(function(a, b){ return a.length - b.length; });
+    let xrObjs = objs.filter(function(a){ return a.startsWith('XR') });
+
+    let requiredObs = ["XRPose", "XRView", "XRFrame", "XRSpace", "XRSession", "XRViewport", "XRViewerPose", "XRWebGLLayer", "XRInputSource", "XRRenderState", "XRSessionEvent", "XRReferenceSpace", "XRRigidTransform", "XRInputSourceArray", "XRInputSourceEvent", "XRReferenceSpaceEvent", "XRBoundedReferenceSpace", "XRInputSourcesChangeEvent"];
+
+    for (let i = 0; i < requiredObs.length; i++) {
+      let prop = requiredObs[i];
+      if (window[prop] === undefined) {
+        return false;
+      }
+    }
+    return true;
+  },
+  expect: true
+
 }
 ];
